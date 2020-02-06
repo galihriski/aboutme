9c0e41a3df51be7011f9382a4045e38dc82a4e50
galihriski <pratamagalihriski9@gmail.com>
2020-02-06 18:10:01 +0700

    add animation movement

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+
+$('.moody, .nav-link, .navbar-brand, .new-button').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 1500);
+});
